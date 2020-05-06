a3e86b00b0d006330ac8d2511c6a8e337b1ce50d
Tim Ermilov <yamalight@gmail.com>
2020-05-06 17:12:15 +0200

    Initial project setup

---
+++ frontend/src/App.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function App() {
+  return <div className="p-4 flex">App</div>;
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,29 @@
+// Require the framework and instantiate it
+const { ApolloServer, gql } = require('apollo-server-fastify');
+const fastify = require('fastify')({ logger: true });
+const { isConnected } = require('./db');
+const graphqlSchema = require('./graphql');
+
+// Declare a route
+fastify.get('/', async (request, reply) => {
+  return { hello: 'world' };
+});
+
+// Run the server!
+const start = async () => {
+  try {
+    // create graphql server
+    const gqlServer = new ApolloServer({
+      schema: graphqlSchema,
+    });
+
+    await isConnected;
+    await fastify.register(gqlServer.createHandler()).listen(8080);
+    fastify.log.info(`server listening on ${fastify.server.address().port}`);
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+};
+
+module.exports = start;
